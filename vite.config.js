e16d1e5d6b5c9c8604d4f518798a37233250a5fd
Sebastian Böhm <sboe0705@icloud.com>
2026-06-23 21:26:03 +0200

    added github.io deployment to GitHub workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/',
 })
